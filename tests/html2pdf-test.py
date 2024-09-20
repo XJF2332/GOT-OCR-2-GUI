fe8100a1dd4605967a3b5179c748df2969fb97c9
XJF2332 <xujianfeng233@163.com>
2024-09-20 20:35:06 +0800

    Update html2pdf-test.py

--- tests/html2pdf-test.py
+++ tests/html2pdf-test.py
@@ -3,8 +3,14 @@ import pdfkit
 config = pdfkit.configuration(wkhtmltopdf='./wkhtmltopdf/bin/wkhtmltopdf.exe')
 
 options = {
-    'javascript-delay': 10000,  # 延迟时间，确保JavaScript有足够的时间执行
-    'quiet': ''
+    'page-size': 'A4',
+    'margin-top': '10mm',
+    'margin-right': '10mm',
+    'margin-bottom': '10mm',
+    'margin-left': '10mm',
+    'enable-javascript': '',
+    'javascript-delay': 5000,  # 延迟 10 秒，确保 JavaScript 有足够的时间执行
+    'no-stop-slow-scripts': '',  # 不停止慢脚本
 }
 
 pdfkit.from_file('ocr_utf8.html', 'out.pdf', options=options, configuration=config)
