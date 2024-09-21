ece98f05352aaa982a647bf62dfdeb4a7b03a455
XJF2332 <xujianfeng233@163.com>
2024-09-21 15:38:56 +0800

    fixed pdf rendering in CLI-EN

--- CLI-EN.py
+++ CLI-EN.py
@@ -3,6 +3,7 @@ from transformers import AutoModel, AutoTokenizer
 import pdfkit
 from bs4 import BeautifulSoup
 import re
+import html2pdf
 
 config = pdfkit.configuration(wkhtmltopdf='./wkhtmltopdf/bin/wkhtmltopdf.exe')
 
@@ -48,63 +49,73 @@ def convert_html_encoding(input_file_path, output_file_path):
     with open(output_file_path, 'w', encoding='utf-8') as file:
         file.write(content)
 
-def extract_style_from_html(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    style_tag = soup.find('style')
-    return style_tag.string if style_tag else ''
+def repalce_html_content(input_file_path, output_file_path):
+    pattern = r'https://cdn.jsdelivr.net/npm/mathpix-markdown-it@1.3.6/es5/bundle.js'
+    replacement = 'markdown-it.js'
+    with open(input_file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    new_html_content = re.sub(pattern, replacement, content)
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        file.write(new_html_content)
 
 
-def extract_const_text_from_script(script_content):
-    # 使用正则表达式来匹配所有被双引号包围的字符串，并保留换行符
-    string_pattern = r'(?<!\\)"(.*?)(?<!\\)"'
-    matches = re.findall(string_pattern, script_content, re.DOTALL)
-    # 将所有匹配的字符串连接起来，并替换掉转义引号
-    const_text = ''.join(matches).replace('\\"', '"')
-    # 替换JavaScript的换行符为HTML的换行符
-    const_text = const_text.replace('\\n', '<br>')
-    return const_text
+# def extract_style_from_html(html_content):
+#     soup = BeautifulSoup(html_content, 'html.parser')
+#     style_tag = soup.find('style')
+#     return style_tag.string if style_tag else ''
 
 
-def const_text_to_pdf(input_html_path, output_pdf_path):
-    try:
-        with open(input_html_path, 'r', encoding='utf-8') as file:
-            html_content = file.read()
+# def extract_const_text_from_script(script_content):
+#     # 使用正则表达式来匹配所有被双引号包围的字符串，并保留换行符
+#     string_pattern = r'(?<!\\)"(.*?)(?<!\\)"'
+#     matches = re.findall(string_pattern, script_content, re.DOTALL)
+#     # 将所有匹配的字符串连接起来，并替换掉转义引号
+#     const_text = ''.join(matches).replace('\\"', '"')
+#     # 替换JavaScript的换行符为HTML的换行符
+#     const_text = const_text.replace('\\n', '<br>')
+#     return const_text
 
-        # 提取<style>标签内容
-        style_content = extract_style_from_html(html_content)
 
-        # 提取<script>标签内容
-        soup = BeautifulSoup(html_content, 'html.parser')
-        script_tag = soup.find('script')
-        script_content = script_tag.string if script_tag else ''
-
-        # 提取const text内容
-        const_text = extract_const_text_from_script(script_content)
-
-        # 构建完整的HTML文档，包含提取的<style>标签和const text内容
-        styled_text = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="UTF-8">
-            <style>
-                {style_content}
-            </style>
-        </head>
-        <body>
-            <pre>{const_text}</pre>
-        </body>
-        </html>
-        """
-
-        if not os.path.exists("./result"):
-            os.makedirs("./result")
-
-        # 将const text内容转换为PDF
-        pdfkit.from_string(styled_text, output_pdf_path, configuration=config)
-        return f"转换完成，PDF文件已保存为：{output_pdf_path}"
-    except Exception as e:
-        return f"转换失败：{e}"
+# def const_text_to_pdf(input_html_path, output_pdf_path):
+#     try:
+#         with open(input_html_path, 'r', encoding='utf-8') as file:
+#             html_content = file.read()
+#
+#         # 提取<style>标签内容
+#         style_content = extract_style_from_html(html_content)
+#
+#         # 提取<script>标签内容
+#         soup = BeautifulSoup(html_content, 'html.parser')
+#         script_tag = soup.find('script')
+#         script_content = script_tag.string if script_tag else ''
+#
+#         # 提取const text内容
+#         const_text = extract_const_text_from_script(script_content)
+#
+#         # 构建完整的HTML文档，包含提取的<style>标签和const text内容
+#         styled_text = f"""
+#         <!DOCTYPE html>
+#         <html>
+#         <head>
+#             <meta charset="UTF-8">
+#             <style>
+#                 {style_content}
+#             </style>
+#         </head>
+#         <body>
+#             <pre>{const_text}</pre>
+#         </body>
+#         </html>
+#         """
+#
+#         if not os.path.exists("./result"):
+#             os.makedirs("./result")
+#
+#         # 将const text内容转换为PDF
+#         pdfkit.from_string(styled_text, output_pdf_path, configuration=config)
+#         return f"转换完成，PDF文件已保存为：{output_pdf_path}"
+#     except Exception as e:
+#         return f"转换失败：{e}"
 
 
 # do OCR
@@ -204,7 +215,8 @@ def do_ocr(image):
         print("-"*10)
         conv = input("Convert HTML to PDF?(y/n): ")
         if conv.lower() == 'y':
-            const_text_to_pdf(html_utf8_path, f"./result/{img_name_no_ext}.pdf")
+            repalce_html_content(html_utf8_path, html_utf8_path)
+            html2pdf.output_pdf(html_utf8_path, f"./result/{img_name_no_ext}.pdf")
             print(f"Converted PDF saved to {img_name_no_ext}.pdf")
         elif conv.lower() == 'n':
             print("Exiting program...")
