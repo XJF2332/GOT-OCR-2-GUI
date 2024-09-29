0e2ccfa966935c81b08237691e614282972d3e95
XJF2332 <xujianfeng233@163.com>
2024-09-29 17:05:31 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -16,7 +16,6 @@ print(local["import_libs"])
 from transformers import AutoModel, AutoTokenizer
 import gradio as gr
 import os
-import re
 import scripts.html2pdf as html2pdf
 
 # 加载模型
@@ -46,39 +45,6 @@ theme = gr.themes.Base(
 )
 
 
-# 将文件以GB2312编码读取，并以UTF-8编码写入新文件
-def convert_html_encoding(input_file_path, output_file_path):
-    # 以GB2312编码读取文件
-    with open(input_file_path, 'r', encoding='gb2312') as file:
-        content = file.read()
-
-    # 以UTF-8写入内容到新文件
-    with open(output_file_path, 'w', encoding='utf-8') as file:
-        file.write(content)
-
-
-# 替换html文件中的内容
-def replace_html_content(input_file_path, output_file_path):
-    pattern = r'https://cdn.jsdelivr.net/npm/mathpix-markdown-it@1.3.6/es5/bundle.js'
-    replacement = 'markdown-it.js'
-    with open(input_file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-    new_html_content = re.sub(pattern, replacement, content)
-    with open(output_file_path, 'w', encoding='utf-8') as file:
-        file.write(new_html_content)
-
-
-# 将html文件转换为pdf文件
-def func_save_as_pdf(html_utf8_path, html_utf8_local_path, pdf_path):
-    # 替换html文件中的内容
-    replace_html_content(html_utf8_path, html_utf8_local_path)
-    # 将html文件转换为pdf文件
-    html2pdf.output_pdf(html_utf8_local_path, pdf_path)
-    # 打印pdf保存成功
-    success = local["pdf_save_success"].format(pdf_path=pdf_path)
-    return success
-
-
 # 构建文件名
 def build_name(img_name):
     # 构建html文件名
@@ -90,10 +56,11 @@ def build_name(img_name):
 # 提交文件名
 def func_submit_name(img_name):
     # 构建html文件名
+    html_gb2312_path = os.path.join("result", f"{img_name}-gb2312.html")
     html_utf8_path = os.path.join("result", f"{img_name}-utf8.html")
     html_utf8_local_path = os.path.join("result", f"{img_name}-utf8-local.html")
     pdf_path = os.path.join("result", f"{img_name}-utf8.pdf")
-    return html_utf8_path, html_utf8_local_path, pdf_path
+    return html_gb2312_path, html_utf8_path, html_utf8_local_path, pdf_path
 
 
 # 进行OCR识别
@@ -106,8 +73,7 @@ def ocr(image, fine_grained_box_x1, fine_grained_box_y1, fine_grained_box_x2,
     res = local["ocr_mode_none"]
 
     # 构建html文件名
-    html_gb2312_path = build_name(img_name)[0]
-    html_utf8_path = build_name(img_name)[1]
+    html_gb2312_path, html_utf8_path = build_name(img_name)
 
     # 如果result文件夹不存在，则创建
     if not os.path.exists("result"):
@@ -134,7 +100,7 @@ def ocr(image, fine_grained_box_x1, fine_grained_box_y1, fine_grained_box_x2,
         # 进行渲染
         model.chat(tokenizer, image, ocr_type='format', render=True, save_render_file=html_gb2312_path)
         # 将文件以GB2312编码读取，并以UTF-8编码写入新文件
-        convert_html_encoding(html_gb2312_path, html_utf8_path)
+        html2pdf.convert_html_encoding(html_gb2312_path, html_utf8_path)
         # 打印渲染保存成功
         res = local["render_save_success"].format(html_utf8_path=html_utf8_path, html_gb2312_path=html_gb2312_path)
     return res
@@ -149,18 +115,18 @@ with gr.Blocks(theme=theme) as demo:
                 # Fine-grained 设置
                 gr.Markdown(local["fine_grained_settings"])
                 with gr.Row():
-                        fine_grained_box_x1 = gr.Number(label=local["fine_grained_box_x1"], value=0)
-                        fine_grained_box_y1 = gr.Number(label=local["fine_grained_box_y1"], value=0)
-                        fine_grained_box_x2 = gr.Number(label=local["fine_grained_box_x2"], value=0)
-                        fine_grained_box_y2 = gr.Number(label=local["fine_grained_box_y2"], value=0)
+                    fine_grained_box_x1 = gr.Number(label=local["fine_grained_box_x1"], value=0)
+                    fine_grained_box_y1 = gr.Number(label=local["fine_grained_box_y1"], value=0)
+                    fine_grained_box_x2 = gr.Number(label=local["fine_grained_box_x2"], value=0)
+                    fine_grained_box_y2 = gr.Number(label=local["fine_grained_box_y2"], value=0)
                 fine_grained_color = gr.Dropdown(choices=["red", "green", "blue"], label=local["fine_grained_color"])
             with gr.Column():
                 # 渲染设置
                 gr.Markdown(local["render_settings"])
                 with gr.Row():
                     img_name = gr.Textbox(label=local["img_name"], value="ocr")
-                    submit_name = gr.Button(local["submit_img_name"], visible=False)
                 with gr.Row():
+                    html_gb2312_path = gr.Textbox(visible=False, value=os.path.join("result", "ocr-gb2312.html"))
                     html_utf8_path = gr.Textbox(label=local["html_file_path"],
                                                 value=os.path.join("result", "ocr-utf8.html"), interactive=False)
                     html_utf8_local_path = gr.Textbox(label=local["html_local_file_path"],
@@ -168,7 +134,8 @@ with gr.Blocks(theme=theme) as demo:
                                                       interactive=False)
                     pdf_path = gr.Textbox(label=local["pdf_file_path"], value=os.path.join("result", "ocr-utf8.pdf"),
                                           interactive=False)
-                    img_name.change(func_submit_name, inputs=[img_name], outputs=[html_utf8_path, html_utf8_local_path, pdf_path])
+                    img_name.change(func_submit_name, inputs=[img_name],
+                                    outputs=[html_gb2312_path, html_utf8_path, html_utf8_local_path, pdf_path])
         # OCR设置
         gr.Markdown(local["ocr_settings"])
 
@@ -199,17 +166,12 @@ with gr.Blocks(theme=theme) as demo:
                 fine_grained_box_y2, ocr_mode, fine_grained_color, img_name],
         outputs=result
     )
+
     # 点击保存为pdf
     save_as_pdf.click(
-        fn=func_save_as_pdf,
-        inputs=[html_utf8_path, html_utf8_local_path, pdf_path],
+        fn=html2pdf.all_in_one,
+        inputs=[html_gb2312_path, html_utf8_path, html_utf8_local_path, pdf_path],
         outputs=save_as_pdf_info
-    )
-    # 点击提交图片名
-    submit_name.click(
-        fn=func_submit_name,
-        inputs=[img_name],
-        outputs=[html_utf8_path, html_utf8_local_path, pdf_path]
     )
 
 # 启动gradio界面
