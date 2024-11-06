13047ec0e9b3242898a9a96c8b2e9478f366f4f7
XJF2332 <xujianfeng233@163.com>
2024-11-06 12:47:27 +0800

    Add comments

--- scripts/HTML2PDF.py
+++ scripts/HTML2PDF.py
@@ -7,24 +7,15 @@ import time
 import re
 
 
-def convert_html_encoding(html_gb2312_path, html_utf8_path):
+def convert_html_encoding(html_gb2312_path: str, html_utf8_path: str):
     """
-    将GB2312编码的HTML文件转换为UTF-8编码的HTML文件。
+    将GB2312编码的HTML文件转换为UTF-8编码。
+    Args:
+        html_gb2312_path: GB2312编码的HTML文件路径。
+        html_utf8_path: 转换后的UTF-8编码的HTML文件路径。
 
-    参数:
-    - html_gb2312_path: str
-        GB2312编码的HTML文件的路径。
-    - html_utf8_path: str
-        转换为UTF-8编码后HTML文件的保存路径。
-
-    功能:
-    - 读取指定路径的GB2312编码的HTML文件。
-    - 将读取的内容转换为UTF-8编码。
-    - 将转换后的内容写入到新的HTML文件中。
-
-    注意:
-    - 确保输入文件确实使用GB2312编码，否则转换可能会失败。
-    - 输出文件将使用UTF-8编码，这是现代Web标准推荐使用的编码。
+    Returns:
+        None
     """
     try:
         # gb2312
@@ -39,20 +30,15 @@ def convert_html_encoding(html_gb2312_path, html_utf8_path):
         print(f"[Error-HTML2PDF.convert_html_encoding] {e}")
 
 
-def repalce_html_content(html_utf8_path, html_utf8_local_path):
+def repalce_html_content(html_utf8_path: str, html_utf8_local_path: str):
     """
-    替换HTML文件中的内容。
+    替换HTML内容
+    Args:
+        html_utf8_path: UTF-8编码的HTML文件路径。
+        html_utf8_local_path: 替换后的HTML文件路径。
 
-    参数:
-    - html_utf8_path: str
-        输入的UTF-8编码的HTML文件路径。
-    - html_utf8_local_path: str
-        替换内容后的HTML文件保存路径。
-
-    功能:
-    - 读取UTF-8编码的HTML文件内容。
-    - 替换HTML文件中的特定内容。
-    - 将替换后的内容写入新的HTML文件。
+    Returns:
+        None
     """
     try:
         print(f"[Info-HTML2PDF.repalce_html_content] 正在准备替换：{html_utf8_path}")
@@ -75,28 +61,17 @@ def repalce_html_content(html_utf8_path, html_utf8_local_path):
         print(f"[Error-HTML2PDF.repalce_html_content] {e}")
 
 
-def output_pdf(html_path, pdf_path, waiting_time, wait=False):
+def output_pdf(html_path: str, pdf_path: str, waiting_time: int, wait: bool = False):
     """
     将HTML文件转换为PDF文件。
+    Args:
+        html_path: HTML文件路径。
+        pdf_path: PDF文件路径。
+        waiting_time: 等待时间。
+        wait: 是否等待。
 
-    参数:
-    - html_path: str
-        要转换的HTML文件的路径。
-    - pdf_path: str
-        输出PDF文件的保存路径。
-    - waiting_time: int
-        等待时间（秒）。
-    - wait: bool
-        是否等待页面加载完成。默认为False
-
-    功能:
-    - 使用Edge浏览器在无头模式下打开HTML文件。
-    - 等待页面加载完成。
-    - 执行打印命令，将页面内容转换为PDF格式。
-    - 将生成的PDF数据保存到指定的文件路径。
-
-    注意:
-    - 需要事先在本地安装EdgeDriver，并设置正确的路径。
+    Returns:
+        None
     """
     try:
         print(f"[Info-HTML2PDF.output_pdf] 正在转换 PDF ：{html_path}")
@@ -149,28 +124,20 @@ def output_pdf(html_path, pdf_path, waiting_time, wait=False):
         print(f"[Error-HTML2PDF.output_pdf] {e}")
 
 
-def all_in_one(html_gb2312_path, html_utf8_path, html_utf8_local_path, pdf_path, wait, waiting_time):
+def all_in_one(html_gb2312_path: str, html_utf8_path: str, html_utf8_local_path: str, pdf_path: str, wait: bool,
+               waiting_time: int):
     """
-    将GB2312编码的HTML文件转换为UTF-8编码，替换内容，并最终输出为PDF文件。
+    将GB2312编码的HTML文件转换为UTF-8编码，并替换内容，最后转换为PDF文件。
+    Args:
+        html_gb2312_path: GB2312编码的HTML文件路径。
+        html_utf8_path: UTF-8编码的HTML文件路径。
+        html_utf8_local_path: 替换后的HTML文件路径。
+        pdf_path: PDF文件路径。
+        wait: 是否等待。
+        waiting_time: 等待时间。
 
-    参数:
-    - html_gb2312_path: str
-        GB2312编码的原始HTML文件的路径。
-    - html_utf8_path: str
-        转换为UTF-8编码后的HTML文件的临时保存路径。
-    - html_utf8_local_path: str
-        替换内容后的UTF-8编码HTML文件的本地保存路径。
-    - pdf_path: str
-        最终生成的PDF文件的保存路径。
-    - wait: bool
-        是否等待页面加载完成。
-    - time: int
-        等待时间（秒）。
-
-    功能:
-    - 使用`convert_html_encoding`函数将GB2312编码的HTML文件转换为UTF-8编码。
-    - 使用`replace_html_content`函数替换UTF-8编码HTML文件中的特定内容。
-    - 使用`output_pdf`函数将替换内容后的HTML文件输出为PDF文件。
+    Returns:
+        None
     """
     try:
         print(f"[Info-HTML2PDF.all_in_one] 正在转换：{html_gb2312_path}")
