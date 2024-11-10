b7b9228ed0fcfc6fd56fbed92517b7622a6e2ec3
XJF2332 <xujianfeng233@163.com>
2024-11-10 15:33:07 +0800

    move files

---
+++ gguf/Img2Tensor.py
@@ -0,0 +1,13 @@
+import torchvision.transforms as transforms
+import torch
+import cv2 as cv
+
+img = cv.imread("imgs\Test.png")
+print(img.shape)
+
+transform = transforms.ToTensor()
+tensor = transform(img)
+print(tensor.shape)
+
+# save
+torch.save(tensor, "../tests/imgs/Test.pt")
